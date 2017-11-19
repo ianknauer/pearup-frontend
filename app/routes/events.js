c9adc1e9a59b5ba11ab68e309e80015921a8f675
ian knauer <ian.knauer@gmail.com>
2017-11-19 11:50:14 -0800

    start of homepage code

--- app/routes/events.js
+++ app/routes/events.js
@@ -1,8 +1,19 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import macros from 'ember-macaroni'; // imports all the things
 
+const { rejectFromCollectionByValue } = macros;
 const { service } = Ember.inject;
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+
+  sessionUser: Ember.inject.service('session-user'),
+
+  model(params) {
+    return this.store.findAll('event').then(function(events) {
+      return rejectFromCollectionByValue('events', 'user.id', 1);
+    });
+
+  },
 
 });
